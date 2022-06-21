abca32c858627c347953bf176bcd90b0a8aab72c
Sanjeev DITS <sanjeev.kumar@ditstek.com>
2022-06-21 17:21:23 +0530

    Removing unused default files procided by CRA and added global styled

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./redux/store";
+import GlobalStyle from "./components/GlobalStyles";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle/>
     <Provider store={store}>
       <App />
     </Provider>
